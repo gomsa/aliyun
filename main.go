a83203f8e3f0c2ebde85356949a2929a441d3d5f
bigrocs <bigrocs@qq.com>
2019-06-16 19:20:48 +0800

    自动提交 git 代码

--- main.go
+++ main.go
@@ -6,18 +6,9 @@ import (
 	micro "github.com/micro/go-micro"
 	k8s "github.com/micro/kubernetes/go/micro"
 
-	// 执行数据迁移
-	_ "github.com/gomsa/user-srv/database/migrations"
 
-	"github.com/gomsa/user-srv/hander"
-	authPB "github.com/gomsa/user-srv/proto/auth"
-	permissionPB "github.com/gomsa/user-srv/proto/permission"
-	rolePB "github.com/gomsa/user-srv/proto/role"
-	userPB "github.com/gomsa/user-srv/proto/user"
-	casbinPB "github.com/gomsa/user-srv/proto/casbin"
-	db "github.com/gomsa/user-srv/providers/database"
-	"github.com/gomsa/user-srv/providers/casbin"
-	"github.com/gomsa/user-srv/service"
+	pb "github.com/gomsa/aliyun/proto/aliyun"
+
 )
 
 func main() {
@@ -28,23 +19,11 @@ func main() {
 	srv.Init()
 
 	// 用户服务实现
-	repo := &service.UserRepository{db.DB}
-	userPB.RegisterUsersHandler(srv.Server(), &hander.User{repo})
+	pb.RegisterAliyunHandler(srv.Server(), &hander.Aliyun{
+		AccessKeyId:ACCESS_KEY_ID,
+		AccessKeySecret:ACCESS_KEY_SECRET,
+	})
 
-	// token 服务实现
-	token := &service.TokenService{}
-	authPB.RegisterAuthHandler(srv.Server(), &hander.Auth{token, repo})
-
-	// 权限服务实现
-	prepo := &service.PermissionRepository{db.DB}
-	permissionPB.RegisterPermissionsHandler(srv.Server(), &hander.Permission{prepo})
-
-	// 角色服务实现
-	rrepo := &service.RoleRepository{db.DB}
-	rolePB.RegisterRolesHandler(srv.Server(), &hander.Role{rrepo})
-
-	// 权限管理服务实现
-	casbinPB.RegisterCasbinHandler(srv.Server(),&hander.Casbin{casbin.Enforcer})
 	// Run the server
 	if err := srv.Run(); err != nil {
 		log.Log(err)
